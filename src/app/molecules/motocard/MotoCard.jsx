5f04edd8f0e539eeddb12190752ed5cf66a31a64
maniratnagupta <ymrg3307@gmail.com>
2024-11-03 02:06:51 +0530

    call to action's top text between border implemented

--- src/app/molecules/motocard/MotoCard.jsx
+++ src/app/molecules/motocard/MotoCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './MotoCard.module.css'
 import Button from '@/app/atoms/button/Button'
+
 const MotoCard = () => {
     return (
         <div className={styles.ctaContainer}>
+            <div className={styles.topLineContainer}>
+                <div className={styles.halfBorder}></div>
+                <span className={styles.ctaHeading}>OUR JOURNALISM</span>
+                <div className={styles.halfBorder}></div>
+            </div>
             <div className={styles.ctaContentContainer}>
-                <span className={styles.moto}>OUR JOURNALISM</span>
                 <p>The Morning Context makes you smarter with original, accurate and independent journalism.</p>
                 <Button text='SUBSCRIBE' />
             </div>
