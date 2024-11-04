df9c92897840e858634909747f0fb9e7278cd9d1
maniratnagupta <ymrg3307@gmail.com>
2024-11-04 11:54:08 +0530

    header section added with proper nav and button elements

--- src/app/organisms/header/Header.jsx
+++ src/app/organisms/header/Header.jsx
@@ -1,21 +1,45 @@
 import { List, Search } from "react-bootstrap-icons"
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from "./header.module.css"
 import Button from "@/app/atoms/button/Button"
+
 const Header = () => {
     return (
-        <header className={styles.headerSection}>
-            <div className={styles.LeftHeaderContainer}>
-                <List size={23} />
+        <header className={styles.headerSection} role="banner">
+            <nav className={styles.LeftHeaderContainer} aria-label="Main navigation">
+                <button
+                    aria-label="Open menu"
+                    className={styles.menuButton}
+                >
+                    <List size={23} />
+                </button>
                 <div className={styles.compNameLogoWrapper}>
                     <div className={styles.logoContainer}>
                     </div>
-                    <Image className={styles.compNameImg} src='/THE MORNING CONTEXT.svg' alt='logo' width={59} height={27} />
+                    <Link href="/">
+                        <Image
+                            className={styles.compNameImg}
+                            src='/THE MORNING CONTEXT.svg'
+                            alt='The Morning Context Logo'
+                            width={59}
+                            height={27}
+                            priority
+                        />
+                    </Link>
                 </div>
-            </div>
+            </nav>
             <div className={styles.rightSectionContainer}>
-                <Search size={18} className={styles.searchIcon} />
-                <Button text='SIGN UP'/>
+                <button
+                    aria-label="Search"
+                    className={styles.searchButton}
+                >
+                    <Search size={18} className={styles.searchIcon} />
+                </button>
+                <Button
+                    text='SIGN UP'
+                    aria-label="Sign up for an account"
+                />
             </div>
         </header>
     )
